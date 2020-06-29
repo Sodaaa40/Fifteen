5e4a4f8224152603f26f6c5e875da24c1a9a9ec4
Sorokovikh Darya <sorokovihd2001@icloud.com>
2020-06-29 16:59:02 +0700

    Format sources according to codestyle

--- src/main.cpp
+++ src/main.cpp
@@ -4,102 +4,83 @@ using namespace sf;
 
 int main()
 {
-	RenderWindow window(VideoMode(256, 256), "simple 15-puzzle game");
+    RenderWindow window(VideoMode(256, 256), "simple 15-puzzle game");
 
-	Texture texture;
-	Texture menu;
-	texture.loadFromFile("./bin/images/15.png");
+    Texture texture;
+    Texture menu;
+    texture.loadFromFile("./bin/images/15.png");
 
-	int blocksize = 64;
-	game fifteen;
-	//fifteen.shuffle();
-	Sprite sprite[16];
+    int blocksize = 64;
+    game fifteen;
+    // fifteen.shuffle();
+    Sprite sprite[16];
 
+    int n = 0;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++) {
+            sprite[n].setTexture(texture);
+            sprite[n].setTextureRect(IntRect(
+                    i * blocksize, j * blocksize, blocksize, blocksize));
+            n++;
+        }
 
-	int n = 0;
-	for (int i = 0; i < 4; i++)
-		for (int j = 0; j < 4; j++)
-		{
-			sprite[n].setTexture(texture);
-			sprite[n].setTextureRect(IntRect(i*blocksize, j*blocksize, blocksize, blocksize));
-			n++;
-		}
-		
-	
+    while (window.isOpen()) {
+        Event event;
 
-	while (window.isOpen())
-	{
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
 
+            if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    Vector2i position = Mouse::getPosition(window);
 
-		Event event;
+                    int vert = position.x / blocksize;
+                    int hor = position.y / blocksize;
 
-		while (window.pollEvent(event))
-		{
+                    fifteen.move(fifteen.getFieldCell(hor, vert));
+                }
+            }
+        }
 
-			if (event.type == Event::Closed)
-			    window.close();
+        window.clear(Color::White);
 
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++) {
+                int num = fifteen.getFieldCell(j, i) - 1;
 
-			if (event.type == Event::MouseButtonPressed)
-			{
+                sprite[num].setPosition(i * blocksize, j * blocksize);
 
-				if (event.mouseButton.button == Mouse::Left)
-				{
-					Vector2i position = Mouse::getPosition(window);
+                window.draw(sprite[num]);
+            }
 
-					int vert = position.x / blocksize;
-					int hor = position.y / blocksize;
+        window.display();
 
-					fifteen.move(fifteen.getFieldCell(hor, vert));
-				}
-					
-			}
-				
-		}
+        if (fifteen.checkWin() == 0) {
+            RenderWindow subwindow(VideoMode(490, 167), "menu");
+            menu.loadFromFile("./bin/images/re.png");
+            Sprite menusprite(menu);
+            subwindow.clear(Color::White);
+            subwindow.draw(menusprite);
+            subwindow.display();
+            while (fifteen.checkWin() == 0) {
+                if (subwindow.waitEvent(event)) {
+                    if (event.type == Event::MouseButtonPressed) {
+                        if (event.mouseButton.button == Mouse::Left) {
+                            Vector2i position = Mouse::getPosition(subwindow);
+                            int hor = position.y;
+                            if (hor > 83) {
+                                return 0;
+                            } else {
+                                fifteen.shuffle();
+                                subwindow.close();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-		window.clear(Color::White);
-
-
-		for (int i = 0; i < 4; i++)
-			for (int j = 0; j < 4; j++)
-			{
-				int num = fifteen.getFieldCell(j, i) - 1;
-
-				sprite[num].setPosition(i*blocksize, j*blocksize);
-
-				window.draw(sprite[num]);
-			}
-		
-
-
-		window.display();
-		
-		if(fifteen.checkWin() == 0){
-			RenderWindow subwindow(VideoMode(490, 167), "menu");
-			menu.loadFromFile("./bin/images/re.png");
-			Sprite menusprite(menu);
-			subwindow.clear(Color::White);
-			subwindow.draw(menusprite);
-			subwindow.display();
-			while(fifteen.checkWin() == 0){
-			if(subwindow.waitEvent(event)){
-				if(event.type == Event::MouseButtonPressed){
-					if(event.mouseButton.button == Mouse::Left){
-						Vector2i position = Mouse::getPosition(subwindow);
-						int hor = position.y;
-						if(hor > 83){
-							return 0;
-						}else{
-							fifteen.shuffle();
-							subwindow.close();
-						}
-					}
-				}
-			}
-		}
-		}
-
-	}
-	
-	return 0;
+    return 0;
 }
